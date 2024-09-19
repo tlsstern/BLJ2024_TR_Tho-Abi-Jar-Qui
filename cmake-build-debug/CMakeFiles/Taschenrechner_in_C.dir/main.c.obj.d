21a3be6112cfccc471a8a08e60299478686cdc5b
Jaradrj <jara.derooij@edu.tbz.ch>
2024-09-19 13:26:02 +0200

    do main

---
+++ cmake-build-debug/CMakeFiles/Taschenrechner_in_C.dir/main.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/Taschenrechner_in_C.dir/main.c.obj: \
+ C:\Projects\blj2024-jar-der\C\2024\BLJ2024_TR_Tho-Abi-Jar-Qui\main.c \
+ C:/MyTools/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/stdio.h \
+ C:/MyTools/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/MyTools/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/corecrt.h \
+ C:/MyTools/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/_mingw.h \
+ C:/MyTools/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/MyTools/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/MyTools/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
+ C:/MyTools/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/MyTools/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/MyTools/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/MyTools/CLion\ 2024.2.1/bin/mingw/x86_64-w64-mingw32/include/sec_api/stdio_s.h
--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    int choice;
+    printf("CALCULATOR\n");
+    printf("What do you want to do? (input Number)\n");
+    printf("1. Addition\t\t2. Subtraction\t\t3. Division\n");
+    printf("4. Multiplication\t5. Exponential\t\t6. Root\n");
+    printf("7. Factoral\t\t8.Geometry");
+    scanf("c", &choice);
+    switch (choice) {
+        case '1': break;
+        case '2': break;
+        case '3': break;
+        case '4': break;
+        case '5': break;
+        case '6': break;
+        case '7': break;
+    }
     return 0;
 }
