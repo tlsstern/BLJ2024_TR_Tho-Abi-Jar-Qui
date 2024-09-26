00e0e8c6ddb8f7d8956d12499f61c72575c7d0c9
tlsstern <thomas.stern@edu.tbz.ch>
2024-09-26 10:04:59 +0200

    Fixed that if you click anything but a number it breaks. Now it brings you back

--- Taschenrechner/geometrie.c
+++ Taschenrechner/geometrie.c
@@ -4,172 +4,266 @@
 #include <stdio.h>
 #include "geometrie.h"
 
-void Area();
-void circle();
-void square();
-void rectangle();
-void triangle();
-void trapezoid();
-void Volume();
-void ball();
-void cube();
-void cuboid();
-void cylinder();
-void cone();
-
-
 void geometry() {
-  int choice;
-  printf("What do you want to do?\n");
-  printf("1. Area\t\t2. Volume\n");
-  scanf("%d", &choice);
-  if (choice == 1) {
-    Area();
-  }else if (choice == 2) {
-    Volume();
-  } else if (choice != 1 && choice != 2) {
-    printf("Wrong entry\n");
-  }
-}
-void Area(){
     int choice;
+    start:
+    printf("What do you want to do?\n");
+    printf("1. Area\t\t2. Volume\n");
+    scanf("%d", &choice);
+    if (choice == 1) {
+        Area();
+    } else if (choice == 2) {
+        Volume();
+    } else if (choice != 1 && choice != 2) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto start;
+    }
+}
+
+void Area() {
+    int choice;
+    start_choice:
     printf("What Area do you want to calculate?\n");
     printf("1. circle\t\t2. square\t\t3. rectangle\n");
     printf("4. triangle\t\t5. trapezoid\n");
     scanf("%d", &choice);
 
-  switch (choice) {
-      case 1: circle();
-         break;
-    case 2: square();
-         break;
-    case 3: rectangle();
-          break;
-    case 4: triangle();
-        break;
-    case 5: trapezoid();
-        break;
-    default: printf("Wrong entry\n");
-  }
+    switch (choice) {
+        case 1: circle();
+            break;
+        case 2: square();
+            break;
+        case 3: rectangle();
+            break;
+        case 4: triangle();
+            break;
+        case 5: trapezoid();
+            break;
+        default: printf("Wrong entry\n");
+        fflush(stdin);
+        goto start_choice;
+    }
 }
 
-void circle(){
+void circle() {
     float pi = 3.14159;
     float radius;
+    circles:
     printf("Enter radius in cm\n");
-    scanf("%f", &radius);
+    if (scanf("%f", &radius) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto circles;
+    }
     float result = pi * radius * radius;
     printf("The area is %.2f\n", result);
 }
+
 void square() {
     float length;
+    square:
     printf("Enter site length in cm\n");
-    scanf("%f", &length);
+    if (scanf("%f", &length) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto square;
+    }
     float result = length * length;
     printf("The area is %.2f\n", result);
 }
+
 void rectangle() {
     float lengthOne;
     float lengthTwo;
+    rectangle1:
     printf("Enter length of one Site in cm\n");
-    scanf("%f", &lengthOne);
+    if (scanf("%f", &lengthOne) != 1 ) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto rectangle1;
+    }
     printf("Enter length of second Site in cm\n");
-    scanf("%f", &lengthTwo);
+    rectangle2:
+    if (scanf("%f", &lengthTwo) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto rectangle2;
+    }
     float result = lengthOne * lengthTwo;
     printf("The area is %.2f\n", result);
 }
+
 void triangle() {
     float ground;
     float height;
+    triangle1:
     printf("Enter ground length in cm\n");
-    scanf("%f", &ground);
+    if (scanf("%f", &ground) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto triangle1;
+    }
+    tringle2:
     printf("Enter height in cm\n");
-    scanf("%f", &height);
+    if(scanf("%f", &height) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto tringle2;
+    }
     float result = ground * height / 2;
     printf("The area is %.2f\n", result);
 }
+
 void trapezoid() {
     float lengthOne;
     float lengthTwo;
     float height;
+    trapezoid1:
     printf("Enter ground length in cm\n");
-    scanf("%f", &lengthOne);
+    if (scanf("%f", &lengthOne) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto trapezoid1;
+    }
+    trapezoid2:
     printf("Enter top length in cm\n");
-    scanf("%f", &lengthTwo);
+    if (scanf("%f", &lengthTwo) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto trapezoid2;
+    }
+    trapezoid3:
     printf("Enter height in cm\n");
-    scanf("%f", &height);
+    if (scanf("%f", &height) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto trapezoid3;
+    }
     float result = height / 2 * (lengthOne + lengthTwo);
     printf("The area is %.2f\n", result);
 }
 
 void Volume() {
-  int choice;
-  printf("What Volume do you want to calculate?\n");
-  printf("1. ball\t\t2. cube\t\t3. cuboid\n");
-  printf("4. cylinder\t5. cone\n");
-  scanf("%d", &choice);
-  switch (choice) {
-    case 1: ball();
-        break;
-    case 2: cube();
-        break;
-    case 3: cuboid();
-        break;
-    case 4: cylinder();
-        break;
-    case 5: cone();
-        break;
-    default: printf("Wrong entry\n");
-  }
+    int choice;
+    volume:
+    printf("What Volume do you want to calculate?\n");
+    printf("1. ball\t\t2. cube\t\t3. cuboid\n");
+    printf("4. cylinder\t5. cone\n");
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1: ball();
+            break;
+        case 2: cube();
+            break;
+        case 3: cuboid();
+            break;
+        case 4: cylinder();
+            break;
+        case 5: cone();
+            break;
+        default: printf("Wrong entry\n");
+        fflush(stdin);
+        goto volume;
+    }
 }
 
 void ball() {
     float pi = 3.14159;
     float radius;
+    raduis_vol:
     printf("Enter radius in cm\n");
-    scanf("%f", &radius);
-    float result = 4.0/3 * (radius * radius * radius) * pi;
+    if (scanf("%f", &radius) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto raduis_vol;
+    }
+    float result = 4.0 / 3 * (radius * radius * radius) * pi;
     printf("The volume is %.2f\n", result);
 }
+
 void cube() {
     float length;
+    cube:
     printf("Enter site length in cm\n");
-    scanf("%f", &length);
+    if (scanf("%f", &length) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto cube;
+    }
     float result = length * length * length;
     printf("The volume is %.2f\n", result);
 }
+
 void cuboid() {
     float lengthOne;
     float lengthTwo;
     float lengthThree;
+    cuboid_vol1:
     printf("Enter length of one Site in cm\n");
-    scanf("%f", &lengthOne);
+    if (scanf("%f", &lengthOne) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto cuboid_vol1;
+    }
+    cuboid_vol2:
     printf("Enter length of second Site in cm\n");
-    scanf("%f", &lengthTwo);
+    if (scanf("%f", &lengthTwo) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto cuboid_vol2;
+    }
+    cuboid_vol3:
     printf("Enter length of third Site in cm\n");
-    scanf("%f", &lengthThree);
+    if (scanf("%f", &lengthThree) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto cuboid_vol3;
+    }
     float result = lengthOne * lengthTwo * lengthThree;
     printf("The volume is %.2f\n", result);
 }
+
 void cylinder() {
     float pi = 3.14159;
     float radius;
     float height;
+    cylinder_vol1:
     printf("Enter radius in cm\n");
-    scanf("%f", &radius);
+    if (scanf("%f", &radius) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto cylinder_vol1;
+    }
+    cylinder_vol2:
     printf("Enter height in cm\n");
-    scanf("%f", &height);
+    if (scanf("%f", &height) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto cylinder_vol2;
+    }
     float result = pi * radius * radius * height;
     printf("The volume is %.2f\n", result);
 }
+
 void cone() {
     float pi = 3.14159;
     float radius;
     float height;
+    cone_vol1:
     printf("Enter radius in cm\n");
-    scanf("%f", &radius);
+    if (scanf("%f", &radius) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto cone_vol1;
+    }
+    cone_vol2:
     printf("Enter height in cm\n");
-    scanf("%f", &height);
-    float result = 1.0/3 * pi * radius * radius * height;
+    if (scanf("%f", &height) != 1) {
+        printf("Wrong entry\n");
+        fflush(stdin);
+        goto cone_vol2;
+    }
+    float result = 1.0 / 3 * pi * radius * radius * height;
     printf("The volume is %.2f\n", result);
 }
