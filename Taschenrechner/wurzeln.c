371f5664c3c588f36e7ffa02213ca855689144f5
tlsstern <thomas.stern@edu.tbz.ch>
2024-09-26 10:31:41 +0200

    Fixed it removed art which is unneascry

--- Taschenrechner/wurzeln.c
+++ Taschenrechner/wurzeln.c
@@ -7,45 +7,8 @@ void calculate_square_root() {
     printf("Enter number:\n");
     scanf("%lf", &number);
 
-    if (number < 0) {
-        printf("                             =_-___\n");
-        printf("                           o    \\__ \\\n");
-        printf("                          o       __| \\\n");
-        printf("                           o      \\__  \\\n");
-        printf("                             oooo    \\  \\\n");
-        printf("                                      \\  \\\n");
-        printf("        __________________             |   \\\n");
-        printf("       |__________________|             \\   |\n");
-        printf("        \\/\\/\\/\\/\\/\\/\\/\\/\\/     _----_    |   |\n");
-        printf("         \\/\\/\\/\\/\\/\\/\\/\\/     |      \\   |   |\n");
-        printf("          \\/\\/\\/\\/\\/\\/\\/      |       |    |  |\n");
-        printf("           |/\\/\\/\\/\\/\\/|        |       \\__/    |\n");
-        printf("           |/\\/\\/\\/\\/\\/|         __---          |\n");
-        printf("           |/\\/\\/\\/\\/\\/|       /   \\            |\n");
-        printf("                             |     |          |\n");
-        printf("                             |   /            |\n");
-        printf("                             |   \\            |\n");
-        printf("    Try again!               |   | \\          |\n");
-        printf("                             |   |   \\____-----\\\n");
-        printf("                             |   |    \\____-----\n");
-        printf("                              |  |    |          \\\n");
-        printf("                              |  |   |             \\\n");
-        printf("                               \\  \\_|_      |       |\n");
-        printf("                                \\____/  ___/ \\_____/\n");
-        printf("                                   /    /       \\     \\\n");
-        printf("                                 /     /          \\     \\\n");
-        printf("                                /    /              \\    \\\n");
-        printf("                              /    /                  \\    \\\n");
-        printf("                             /   /                      \\   \\\n");
-        printf("                       /\\   /  /                          \\  |\n");
-        printf("                      |  \\/ \\/                              \\/ \\\n");
-        printf("                       \\    |                             __/   |\n");
-        printf("                         \\_/                            /______/\n");
-        printf("The root can't be calculated from a negative number. Please try it again with a positive number.\n");
-    } else {
-        double result = sqrt_custom(number);
-        printf("The root of %.2lf = %.2lf\n", number, result);
-    }
+    double result = sqrt_custom(number);
+    printf("The root of %.2lf = %.2lf\n", number, result);
 }
 
 double sqrt_custom(double number) {
